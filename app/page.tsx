62cdbf8dca463314b830e5d8e879bd81b6c8238f
pandigitalks <59856709+pandigitalks@users.noreply.github.com>
2026-08-01 15:52:29 +0200

    Activate waitlist from homepage setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Marketplace from "./marketplace-v2";
+import { createServerSupabaseClient } from "../lib/supabase/server";
+import WaitlistLanding from "./waitlist/waitlist-landing";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createServerSupabaseClient();
+  const { data } = await supabase.rpc("waitlist_public_stats");
+  if (data?.enabled) return <WaitlistLanding initialStats={data} />;
   return <Marketplace />;
 }
